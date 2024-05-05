4a8adec5e723a7f3b3652a35885dc41b1aa47a9a
soumilid03 <164724606+soumilid03@users.noreply.github.com>
2024-05-05 14:49:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Container bg={"#f8fafd"} maxW={"Container.3xl"} height={"100vh"} p={"0"}>
       <Flex height={"full"}>
-        <Box height={"full"} flex={5} w={["20%", "30%", "20%", "50%", "60%"]}>
+        <Box height={"full"} flex={5} w={["20%", "29%", "20%", "50%", "60%"]}>
           <Main />
         </Box>
       </Flex>
